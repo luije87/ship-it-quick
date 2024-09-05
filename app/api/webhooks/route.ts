53a814a0f166f10fd09c18184c6881c11d2270ea
luije87 <luije87@gmail.com>
2024-09-05 12:28:03 -0400

    refactor: Update stripe.ts and route.ts for Stripe webhook handling

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,66 +1,96 @@
-import type { Stripe } from "stripe";
+import Stripe from "stripe";
+import { stripe } from "@/utils/stripe/config";
+import {
+  upsertProductRecord,
+  upsertPriceRecord,
+  manageSubscriptionStatusChange,
+  deleteProductRecord,
+  deletePriceRecord,
+} from "@/utils/supabase/admin";
 
-import { NextResponse } from "next/server";
-
-import { stripe } from "@/lib/stripe";
+const relevantEvents = new Set([
+  "product.created",
+  "product.updated",
+  "product.deleted",
+  "price.created",
+  "price.updated",
+  "price.deleted",
+  "checkout.session.completed",
+  "customer.subscription.created",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 export async function POST(req: Request) {
+  const body = await req.text();
+  const sig = req.headers.get("stripe-signature") as string;
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
   let event: Stripe.Event;
 
   try {
-    event = stripe.webhooks.constructEvent(
-      await (await req.blob()).text(),
-      req.headers.get("stripe-signature") as string,
-      process.env.STRIPE_WEBHOOK_SECRET as string
-    );
-  } catch (err) {
-    const errorMessage = err instanceof Error ? err.message : "Unknown error";
-    // On error, log and return the error message.
-    if (err! instanceof Error) console.log(err);
-    console.log(`❌ Error message: ${errorMessage}`);
-    return NextResponse.json(
-      { message: `Webhook Error: ${errorMessage}` },
-      { status: 400 }
-    );
+    if (!sig || !webhookSecret)
+      return new Response("Webhook secret not found.", { status: 400 });
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+    console.log(`🔔  Webhook received: ${event.type}`);
+  } catch (err: any) {
+    console.log(`❌ Error message: ${err.message}`);
+    return new Response(`Webhook Error: ${err.message}`, { status: 400 });
   }
 
-  // Successfully constructed event.
-  console.log("✅ Success:", event.id);
-
-  const permittedEvents: string[] = [
-    "checkout.session.completed",
-    "payment_intent.succeeded",
-    "payment_intent.payment_failed",
-  ];
-
-  if (permittedEvents.includes(event.type)) {
-    let data;
-
+  if (relevantEvents.has(event.type)) {
     try {
       switch (event.type) {
+        case "product.created":
+        case "product.updated":
+          await upsertProductRecord(event.data.object as Stripe.Product);
+          break;
+        case "price.created":
+        case "price.updated":
+          await upsertPriceRecord(event.data.object as Stripe.Price);
+          break;
+        case "price.deleted":
+          await deletePriceRecord(event.data.object as Stripe.Price);
+          break;
+        case "product.deleted":
+          await deleteProductRecord(event.data.object as Stripe.Product);
+          break;
+        case "customer.subscription.created":
+        case "customer.subscription.updated":
+        case "customer.subscription.deleted":
+          const subscription = event.data.object as Stripe.Subscription;
+          await manageSubscriptionStatusChange(
+            subscription.id,
+            subscription.customer as string,
+            event.type === "customer.subscription.created"
+          );
+          break;
         case "checkout.session.completed":
-          data = event.data.object as Stripe.Checkout.Session;
-          console.log(`💰 CheckoutSession status: ${data.payment_status}`);
-          break;
-        case "payment_intent.payment_failed":
-          data = event.data.object as Stripe.PaymentIntent;
-          console.log(`❌ Payment failed: ${data.last_payment_error?.message}`);
-          break;
-        case "payment_intent.succeeded":
-          data = event.data.object as Stripe.PaymentIntent;
-          console.log(`💰 PaymentIntent status: ${data.status}`);
+          const checkoutSession = event.data.object as Stripe.Checkout.Session;
+          if (checkoutSession.mode === "subscription") {
+            const subscriptionId = checkoutSession.subscription;
+            await manageSubscriptionStatusChange(
+              subscriptionId as string,
+              checkoutSession.customer as string,
+              true
+            );
+          }
           break;
         default:
-          throw new Error(`Unhandled event: ${event.type}`);
+          throw new Error("Unhandled relevant event!");
       }
     } catch (error) {
       console.log(error);
-      return NextResponse.json(
-        { message: "Webhook handler failed" },
-        { status: 500 }
+      return new Response(
+        "Webhook handler failed. View your Next.js function logs.",
+        {
+          status: 400,
+        }
       );
     }
+  } else {
+    return new Response(`Unsupported event type: ${event.type}`, {
+      status: 400,
+    });
   }
-  // Return a response to acknowledge receipt of the event.
-  return NextResponse.json({ message: "Received" }, { status: 200 });
+  return new Response(JSON.stringify({ received: true }));
 }
--- config/index.ts
+++ config/index.ts
@@ -5,3 +5,4 @@ export const MIN_AMOUNT = 10.0;
 export const MAX_AMOUNT = 5000.0;
 export const AMOUNT_STEP = 5.0;
 export const PROTECTED_ROUTES = ["/dashboard"];
+export const APPLICATION_NAME = "Ship it Quick ⚡️";
